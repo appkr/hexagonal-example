917aa6fc93fd5d01be6a7b07efc36b797d9a266d
appkr <juwonkim@me.com>
2023-07-23 22:49:03 +0900

    added archunit library and hexagonal test

---
+++ src/test/java/com/example/hexagonal/HexagonalTest.java
@@ -0,0 +1,39 @@
+package com.example.hexagonal;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import org.junit.jupiter.api.Test;
+
+public class HexagonalTest {
+
+  static final JavaClasses importedClasses = new ClassFileImporter()
+      .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+      .importPackages("com.example.hexagonal");
+
+  @Test
+  public void domainShouldNotDependOnAnyOtherPackages() {
+    noClasses()
+        .that()
+        .resideInAnyPackage("com.example.hexagonal.domain..")
+        .should()
+        .dependOnClassesThat()
+        .resideInAnyPackage("com.example.hexagonal.usecase..", "com.example.hexagonal.port..", "com.example.hexagonal.adapter..")
+        .because("Domain should not depend on any other packages")
+        .check(importedClasses);
+  }
+
+  @Test
+  public void usecaseAndPortShouldNotDependOnAdapter() {
+    noClasses()
+        .that()
+        .resideInAnyPackage("com.example.hexagonal.usecase..", "com.example.hexagonal.port..")
+        .should()
+        .dependOnClassesThat()
+        .resideInAnyPackage("com.example.hexagonal.adapter..")
+        .because("Application should not depend on any adapter packages")
+        .check(importedClasses);
+  }
+}
