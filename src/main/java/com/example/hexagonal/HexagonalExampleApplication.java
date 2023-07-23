2f0f50a8b510874f0ebaea05d0a3e9e1340da796
appkr <juwonkim@me.com>
2023-07-23 20:46:28 +0900

    new project created from spring initializer

---
+++ src/main/java/com/example/hexagonal/HexagonalExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hexagonal/HexagonalExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
