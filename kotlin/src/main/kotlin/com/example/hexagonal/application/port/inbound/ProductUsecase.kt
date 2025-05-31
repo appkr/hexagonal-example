b709b0592ab39ae9e2339399187f2403013c139f
flex-juwon <juwon@flex.team>
2025-05-31 14:40:59 +0900

    delete kotlin example to make new one

--- kotlin/src/main/kotlin/com/example/hexagonal/application/port/inbound/ProductUsecase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.hexagonal.application.port.inbound
-
-import com.example.hexagonal.application.domain.Product
-import com.example.hexagonal.application.port.dto.ProductDto
-
-interface ProductUsecase {
-    fun getProduct(productId: Long): Product
-    fun updateProduct(productId: Long, dto: ProductDto): Product
-}
--- kotlin/src/main/kotlin/com/example/hexagonal/application/port/outbound/MessagePort.kt
+++
@@ -1,5 +0,0 @@
-package com.example.hexagonal.application.port.outbound
-
-interface MessagePort {
-    fun send(aHypotheticalMessage: Any): Unit
-}
